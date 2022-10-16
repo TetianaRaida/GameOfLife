fa9cf2ba11191a788f08a2279752331aa10d3265
Tetiana <115114869+TetianaRaida@users.noreply.github.com>
2022-10-16 21:37:45 +0200

    Two files uploaded

---
+++ GameIterations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GameOfLife
+{
+    public class GameIterations
+
+    {
+        static void NextIteration(int[,] a, int[,] b, int[,] field)
+        {
+
+        }
+
+        static void SwapIterations<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Live;
+
+
+public class GameOfLife
+
+{
+
+    int[,] Field = new int[25, 25];
+
+
+    public static int Neighbours { get; set; }
+    public static int Cell { get; set; }
+    public static int numberOfCells = 625;
+
+
+    private static int ConditionsOfTheGame()
+    {
+        if ((Neighbours > 3) || (Neighbours < 2))
+            Cell = 0;
+
+        else if ((Neighbours <= 3) || (Neighbours > 2))
+            Cell = 1;
+
+        return Cell;
+    }
+
+
+    static void Main(string[] args)
+
+    {
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
